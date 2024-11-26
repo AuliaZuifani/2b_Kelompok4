0ce5e6b484cab78ddae70ed023e7c871a122a06a
shalsabilla64 <shalsabiel387@gmail.com>
2024-11-26 13:18:20 +0700

    hapus folder mvc-sample

--- mvc-sample-main/app/views/user/create.php
+++
@@ -1,9 +0,0 @@
-<!-- app/views/user/create.php -->
-<h2>Tambah Peminjam Baru</h2>
-<form action="/user/store" method="POST">
-    <label for="id_pinjam">id pinjam:</label>
-    <input type="varchar" name="name" id="name" required>
-    <label for="id_buku">Id buku:</label>
-    <input type="varchar" name="name" id="name" required>
-    <button type="submit">Simpan</button>
-</form>
