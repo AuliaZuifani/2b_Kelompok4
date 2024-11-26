fe9e8d3185e056e32905a539c47ca0dd3743700e
shalsabilla64 <shalsabiel387@gmail.com>
2024-11-26 11:44:54 +0700

    test

--- mvc-sample-main/app/views/user/create.php
+++ mvc-sample-main/app/views/user/create.php
@@ -2,6 +2,7 @@
 <h2>Tambah Peminjam Baru</h2>
 <form action="/user/store" method="POST">
     <label for="id_pinjam">id pinjam:</label>
+    test
     <input type="varchar" name="name" id="name" required>
     <label for="id_buku">Id buku:</label>
     <input type="varchar" name="name" id="name" required>
